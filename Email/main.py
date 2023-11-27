debb2c8d6933cc0f8ce262a9d120171eea3dc18a
Wata <forgotoffcom@gmail.com>
2023-11-27 22:43:30 +0800

    Show last 5 emails

--- Email/main.py
+++ Email/main.py
@@ -1,14 +1,19 @@
 import pandas as pd
+from read_email import read_email
 
-# Given data
-function_arguments = [
-    {"date": "7th Dec 2023, 10am", "summary": "AI Research Seminar (AIRS) Series"},
-    {"date": "8th Dec 2023, 12pm", "summary": "Good food!"}
-]
+emails = read_email()
 
+#--- For demo, can get weiren to forward 5 emails he want to see being summarised ---#
+function_arguments = ray_function(read_email[:4])
+
+# function_arguments = [
+#     {"date": "7th Dec 2023, 10am", "summary": "AI Research Seminar (AIRS) Series"},
+#     {"date": "8th Dec 2023, 12pm", "summary": "Good food!"}
+# ]
+
+
+# Output to excel
 df = pd.DataFrame(function_arguments)
-
-# Save the DataFrame to an Excel file
 excel_filename = "output.xlsx"
 df.to_excel(excel_filename, index=False)
 
